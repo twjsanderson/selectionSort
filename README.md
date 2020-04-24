788e42d67a5ae28b8c4bb24d34db0209446566a9
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2020-04-24 15:35:19 -0400

    completed code and readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Selection Sort Algorithm Implementations in JS
+
+This application is intedend for Junior Developers who are curious about the various implementations of the Selection Sort Algorithm in Javascript. Use this as a study guide to familiarize yourself with 2 iterative and 1 recursive implementations of this algorithm.
+
+## Objectives
+
+1. Show and describe 3 implementations of a Selection Sort Algorithm in JavaScript.
+
+## Run 
+
+1. Move into root directory
+2. run node index.js
+
+
+## Bugs, Issues, Questions
+
+Shoot me an email --> **tom.w.j.sanderson@gmail.com**
+
+## Lead Developer
+
+- Tom Sanderson 
+
+Email: **tom.w.j.sanderson@gmail.com**
+[My Portfolio](https://portfolio-8af66.firebaseapp.com/)
+[LinkedIn](https://www.linkedin.com/in/tom-sanderson-b6bb5084/)
--- index.js
+++ index.js
@@ -1,35 +1,89 @@
 
+/**
+ * Swap Function
+ * Helper function swaps 2 array values in-place
+ * @param {array} array 
+ * @param {number} i 
+ * @param {number} j 
+ */
 const swap = (array, i, j) => {
     let temp = array[i];
     array[i] = array[j];
     array[j] = temp;
 }
 
+/**
+ * Iterative Selection Sort Function
+ * Version 1 
+ * @param {array} array
+ * @return {array} 
+ */
 const selectionSort1 = (array) => {
-    let result = [];
-    while (true) {
-        let j = 0;
+    let result = [],
+        smallest;
 
+    while (true) {
         for (let i = 0; i < array.length; i++) {
-            if (array[j] >= array[i + 1]) {
-                swap(array, j, i + 1);
+            if (array[0] >= array[i + 1]) {
+                swap(array, 0, i + 1);
             }
         }
 
-        let smallest = array.shift();
+        smallest = array.shift();
         result.push(smallest);
 
-        if (!array.length) {
-            return result;
-        }
+        if (!array.length) return result;
     }
 };
 
-const selectionSort2 = (array) => {
-    
+/**
+ * Recursive Selection Sort Function
+ * @param {array} array 
+ * @param {number} startIndex 
+ */
+const selectionSort2 = (array, startIndex) => {  
+
+    // Base case
+    if (startIndex === array.length) return;
+
+    for (let i = startIndex + 1; i < array.length; i++) {
+        if (array[startIndex] > array[i]) {
+            swap(array, startIndex, i);
+        }
+    }
+
+    startIndex++
+    selectionSort2(array, startIndex)
 };
 
+/**
+ * Iterative Selection Sort Function
+ * Version 2
+ * @param {array} array 
+ * @return {array} 
+ */
+const selectionSort3 = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[minIndex] > array[j]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            swap(array, minIndex, i)
+        }
+    }
+    return array;
+};
 
-let array = [-2,-3,5,3,70,6,-2];
+let array1 = [-2,-3,5,3,-70,6,-2];
+console.log('selectionSort1: ', selectionSort1(array1))
 
-console.log(selectionSort(array))
+let array2 = [-2,-3,5,3,-70,6,-2];
+console.log('Before selectionSort2 has been called: ', array2)
+selectionSort2(array2, 0);
+console.log('After selectionSort2 has been called: ', array2)
+
+let array3 = [-2,-3,5,3,-70,6,-2];
+console.log('selectionSort3: ', selectionSort3(array3))
